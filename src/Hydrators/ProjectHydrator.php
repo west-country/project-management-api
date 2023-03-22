22c49865f2484916fcb7986ca950124602d68615
Robin Hodge <mrrobinhodge@gmail.com>
2023-03-22 14:35:39 +0000

    Changes made following review

--- src/Hydrators/ProjectHydrator.php
+++ src/Hydrators/ProjectHydrator.php
@@ -3,21 +3,14 @@
 namespace ProjectManagementApi\Hydrators;
 
 use DateTime;
-
-// these need to be deleted when the classes are created
-use PDO as DatabaseConnection;
-use ProjectManagementApi\Hydrators\ProjectHydratorDemo\Project;
-
-// uncomment the below when ready to replace the above
-// use ProjectManagementApi\DatabaseConnection; 
-// use ProjectManagementApi\Entities\Project; 
+use ProjectManagementApi\DatabaseConnection; 
+use ProjectManagementApi\Entities\Project; 
 
 class ProjectHydrator
 {
     public static function getAllProjects(DatabaseConnection $db)
     {
-        $query = $db->prepare('SELECT id, `name`, client_id, deadline FROM projects');
-        $query->execute();
+        $query = $db->query('SELECT id, `name`, client_id, deadline FROM projects');
         $projectsArray = $query->fetchAll();
         $projectObjectsArray = [];
         foreach ($projectsArray as $projectItem) {
