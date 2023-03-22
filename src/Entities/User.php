43455bc95df9ffebc80d74db2f2aec09bc1a18aa
tauroid <adamjacobs173@googlemail.com>
2023-03-22 15:59:56 +0000

    Ready for PR

---
+++ src/Entities/User.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ProjectManagementApi\Entities\Users;
+
+class User {
+    private int $id;
+    private string $name;
+    private string $avatar;
+    private string $role;
+
+    public function __construct(int $id, string $name, string $avatar, string $role) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->avatar = $avatar;
+        $this->role = $role;
+    }
+
+    public function toAssociativeArray(): array {
+        return [
+            'id' => strval($this->id),
+            'name' => $this->name,
+            'avatar' => $this->avatar,
+            'role' => $this->role
+        ];
+    }
+}
