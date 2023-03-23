71e89d1e54e51a42bccee5d3ccc521558467d05b
Chloe Smart <chloesmartprint@hotmail.com>
2023-03-23 09:02:00 +0000

    made changes

--- src/Tests/ResponseTest.php
+++ src/Tests/ResponseTest.php
@@ -11,15 +11,18 @@ class ResponseTest extends TestCase
 {
     public function testSuccessJsonSerialize()
     {
-        $expectedOutput = ['message' => 'Successfully retrieved projects', 'data' => [['id' => 17, 'name' => 'Overhold', 'client_id' => 7, 'deadline' => '30/06/2022', 'overdue' => true], ['id' => 18, 'name' => 'Wrapsafe', 'client_id' => 18, 'deadline' => '28/03/2024', 'overdue' => false]]];
-        
         $message = 'Successfully retrieved projects';
 
-        $data = [['id' => 17, 'name' => 'Overhold', 'client_id' => 7, 'deadline' => '30/06/2022', 'overdue' => true], ['id' => 18, 'name' => 'Wrapsafe', 'client_id' => 18, 'deadline' => '28/03/2024', 'overdue' => false]];
+        $data = [['id' => 17, 'name' => 'Overhold', 'client_id' => 7, 'deadline' => '30/06/2022', 'overdue' => true], 
+        ['id' => 18, 'name' => 'Wrapsafe', 'client_id' => 18, 'deadline' => '28/03/2024', 'overdue' => false]];
 
         $response = new Response($message, $data);
+
+        $expectedOutput = ['message' => 'Successfully retrieved projects', 'data' => [['id' => 17, 'name' => 'Overhold', 'client_id' => 7, 'deadline' => '30/06/2022', 'overdue' => true], 
+        ['id' => 18, 'name' => 'Wrapsafe', 'client_id' => 18, 'deadline' => '28/03/2024', 'overdue' => false]]];
         
         $actualOutput = $response->jsonSerialize();
+        
         $this->assertEquals($expectedOutput, $actualOutput);
     }    
 }
