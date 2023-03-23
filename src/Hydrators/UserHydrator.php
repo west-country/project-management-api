af221af378ef19ab5072dbabb049f8ec437d5722
Robin Hodge <mrrobinhodge@gmail.com>
2023-03-23 09:20:19 +0000

    Completed hydrator? untested...

---
+++ src/Hydrators/UserHydrator.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ProjectManagementApi\Hydrators;
+
+use ProjectManagementApi\DatabaseConnection;
+use ProjectManagementApi\Entities\User;
+
+class UserHydrator 
+{
+    public static function getUsersByProjectId (DatabaseConnection $db, int $id): array
+    {
+        $query = $db->prepare('SELECT users.id, `name`, avatar, `role` FROM users JOIN project_users ON project_id = :project_id');
+        $query->execute(['project_id'=>$id]);
+        $usersArray = $query->fetchAll();
+        $userObjectsArray = [];
+        foreach ($usersArray as $userItem) {
+            $userObject = new User(
+                $userItem['id'],
+                $userItem['name'],
+                $userItem['avatar'],
+                $userItem['role'],
+            );
+            $userObjectsArray[] = $userObject;
+        }
+        return $userObjectsArray;
+
+    }
+}
