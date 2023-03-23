8f6dcceb2f9a618cfee1ea8baf851e236a5c3f84
Melody Sylvestre <melody.sylvestre@bristol.ac.uk>
2023-03-23 15:55:27 +0000

    Implemented changes from peer review

--- src/Hydrators/ProjectHydrator.php
+++ src/Hydrators/ProjectHydrator.php
@@ -32,25 +32,25 @@ class ProjectHydrator
     {
         $queryProjectAndClient = $db->prepare('SELECT projects.id, projects.name, client_id, clients.name AS client_name, clients.logo AS client_logo, deadline 
                                                 FROM projects INNER JOIN clients 
-                                                ON projects.client_id = clients.id 
-                                                WHERE projects.id=:id');
+                                                ON client_id = clients.id 
+                                                WHERE projects.id = :id');
         $queryProjectAndClient->execute(['id' => $id]);
         $projectArray = $queryProjectAndClient->fetch();
-        if($projectArray === []) {
-            throw new InvalidProjectIdException; 
+        if (empty($projectArray)) {
+            throw new InvalidProjectIdException("Invalid project Id!");
         }
         $users = UserHydrator::getUsersbyProjectId($db, $id);
         $projectArray['users'] = $users;
-        
+
         $project = new Project(
-                    $projectArray['id'], 
-                    $projectArray['name'],
-                    $projectArray['client_id'],
-                    $projectArray['deadline'],
-                    $projectArray['client_name'],
-                    $projectArray['client_logo'], 
-                    $projectArray['users']
-                    );
-        return($project);
+            $projectArray['id'],
+            $projectArray['name'],
+            $projectArray['client_id'],
+            $projectArray['deadline'],
+            $projectArray['client_name'],
+            $projectArray['client_logo'],
+            $projectArray['users']
+        );
+        return $project;
     }
 }
