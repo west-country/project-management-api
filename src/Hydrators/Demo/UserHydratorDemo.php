3fcd0399037b6403d9cb463b1d167fc475b658d0
tauroid <adamjacobs173@googlemail.com>
2023-03-23 10:45:49 +0000

    Split SQL query to multiple lines, fix UserHydratorDemo

--- src/Hydrators/Demo/UserHydratorDemo.php
+++ src/Hydrators/Demo/UserHydratorDemo.php
@@ -3,7 +3,7 @@ require '../../../vendor/autoload.php';
 use ProjectManagementApi\Hydrators\UserHydrator;
 use ProjectManagementApi\DatabaseConnection;
 
-$db = new DatabaseConnection('mysql:host=db; dbname=projectDB', 'root', 'password');
+$db = new DatabaseConnection('mysql:host=db; dbname=project_manager', 'root', 'password');
 
 echo '<pre>';
 print_r(UserHydrator::getUsersByProjectId($db, 6));
--- src/Hydrators/UserHydrator.php
+++ src/Hydrators/UserHydrator.php
@@ -9,19 +9,23 @@ class UserHydrator
 {
     public static function getUsersByProjectId(DatabaseConnection $db, int $id): array
     {
-        $query = $db->prepare('SELECT users.id, `name`, avatar, `role` FROM users JOIN project_users ON users.id = user_id AND project_id = :project_id');
+        $query = $db->prepare(
+            'SELECT users.id, `name`, avatar, `role` '
+                . 'FROM users JOIN project_users '
+                . 'ON users.id = user_id AND project_id = :project_id'
+        );
         $query->execute(['project_id' => $id]);
-        $usersArray = $query->fetchAll();
-        $userObjectsArray = [];
-        foreach ($usersArray as $userItem) {
+        $users = $query->fetchAll();
+        $userObjects = [];
+        foreach ($users as $userItem) {
             $userObject = new User(
                 $userItem['id'],
                 $userItem['name'],
                 $userItem['avatar'],
                 $userItem['role'],
             );
-            $userObjectsArray[] = $userObject;
+            $userObjects[] = $userObject;
         }
-        return $userObjectsArray;
+        return $userObjects;
     }
 }
