1d88ffd986c98f3d4b243ca4e807c4a8063f849d
Bill Turvill <turvill@hotmail.co.uk>
2023-03-23 11:23:34 +0000

    Project.php - added properties and created new method

--- src/Entities/Project.php
+++ src/Entities/Project.php
@@ -4,6 +4,8 @@ namespace ProjectManagementApi\Entities;
 
 use DateTime;
 
+use ProjectManagementApi\User;
+
 class Project
 {
     private int $id;
@@ -11,14 +13,20 @@ class Project
     private int $client_id;
     private ?DateTime $deadline;
     private ?bool $isOverdue;
+    private ?string $client_name;
+    private ?string $client_logo;
+    private ?array $users;
 
-    public function __construct(int $id, string $name, int $client_id, ?DateTime $deadline)
+    public function __construct(int $id, string $name, int $client_id, ?DateTime $deadline, ?string $client_name, ?string $client_logo, ?array $users)
     {
         $this->id = $id;
         $this->name = $name;
         $this->client_id = $client_id;
         $this->deadline = $deadline;
         $this->isOverdue = $this->calculateIsOverdue();
+        $this->client_name = $client_name;
+        $this->$client_logo = $client_logo;
+        $this->users = $users;
     }
 
     private function calculateIsOverdue(): ?bool
@@ -35,6 +43,27 @@ class Project
     public function getIsOverdue(): ?bool
     {
         return $this->isOverdue;
+    }
+
+    public function toAssociativeArrayAllProperties(): array
+    {   
+        $usersArray = $this->users;
+
+        foreach($usersArray as $user){
+            //this method comes from User class
+            $users[] = $user->toAssociativeArray();
+        }
+        
+        return [
+            'id' => strval($this->id),
+            'name' => $this->name, 
+            'client_id' => strval($this->client_id),
+            'deadline' => is_null($this->deadline) ? null : $this->deadline->format('d/m/Y'), 
+            'overdue' => $this->isOverdue,
+            'client_name' => $this->client_name,
+            'client_logo' => $this->client_logo,
+            'user' => $users
+        ];
     }
 
     public function toAssociativeArrayFewerProperties(): array
